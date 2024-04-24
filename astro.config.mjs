3253a1d627004cc71c9c96ea4a3751ea14f8bb3c
Drend <byronthyto@gmail.com>
2024-04-24 16:42:40 -0500

    Fail añadiendo iconos :v | para la proxima

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,17 @@ import mdx from "@astrojs/mdx";
 import yaml from '@rollup/plugin-yaml';
 import sitemap from "@astrojs/sitemap";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [yaml()]
   },
-  integrations: [tailwind(), react(), mdx(), sitemap()]
+  integrations: [tailwind(), react(), mdx(), sitemap(), icon({
+    include: {
+      mdi: ["*"], // (Default) Loads entire Material Design Icon set
+      mdi: ["account"], // Loads only Material Design Icon's "account" SVG
+    },
+  }),]
 });
