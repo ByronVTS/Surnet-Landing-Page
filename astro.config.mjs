3b430dd453056727f339345bda9da6b73f578662
Drend <byronthyto@gmail.com>
2024-04-24 11:24:07 -0500

    Añadido Hero y Quienes somos primera version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
+import yaml from '@rollup/plugin-yaml';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()]
+  },
   integrations: [tailwind(), react(), mdx(), sitemap()]
 });
